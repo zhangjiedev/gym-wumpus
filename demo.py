1518d6cc7074a9c1543238ced8e1676f18802192
jack <jack@dhcp-10-8-008-061.mobile.reshsg.uci.edu>
2020-02-12 00:16:57 -0800

    demo

--- demo.py
+++ demo.py
@@ -1,14 +1,16 @@
 import gym
 import gym_wumpus
 env = gym.make('wumpus-v0')
-for i_episode in range(2000):
+
+for i_episode in range(20000):
     observation = env.reset()
+    print(observation)
     for t in range(100):
         #env.render()
         #print("Observstion:", observation)
         action = env.action_space.sample()
         observation, reward, done, info = env.step(action)
-        if i_episode > 1800 and done:
+        if i_episode > 19800 and done:
             print("Final Score:", env.getScore())
             print("Episode finished after {} timesteps".format(t+1))
             break
